a02a6e99f8f2f184f671c966653c470484099837
AnnaTheYe <azy2006@nyu.edu>
2025-03-13 23:09:54 -0400

    testing adding build.yaml and placeholder functions

--- src/packagepackage/package.py
+++ src/packagepackage/package.py
@@ -6,3 +6,18 @@ def coin():
         return "heads!"
     else:
         return "tails!"
+
+def play_math():
+   pass
+
+
+def play_geo():
+   pass
+
+
+def play_vocab():
+   pass
+
+
+def play_science():
+   pass
